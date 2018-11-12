e559a756031ea2ad75bf62fd5e54db09df33081d
Corey Smith <coreyas@gmail.com>
2018-11-11 23:56:46 -0500

    Add Anti-forgery Token to rendering data from Layout Service

---
+++ src/Feature/Forms/website/Pipelines/LayoutService/TransformPlaceholderElement/AddAntiForgeryToken.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Helpers;
+using CoreySmith.Foundation.LayoutService.ItemRendering;
+using CoreySmith.Foundation.LayoutService.Pipelines.LayoutService.TransformPlaceholderElement;
+using HtmlAgilityPack;
+using Sitecore.Data.Fields;
+
+namespace CoreySmith.Feature.Forms.Pipelines.LayoutService.TransformPlaceholderElement
+{
+  public class AddAntiForgeryToken : TransformPlaceholderElementProcessor
+  {
+    public override void TransformPlaceholderElement(TransformPlaceholderElementPipelineArgs args)
+    {
+      if (!(args.Element is ExtensibleRenderedJsonRendering extensibleRendering)) return;
+      if (!ShouldAddAntiForgeryToken(extensibleRendering)) return;
+
+      var antiForgeryTokenHtml = AntiForgery.GetHtml();
+      var (name, value) = ParseAntiForgeryToken(antiForgeryTokenHtml);
+      args.Result.antiForgeryToken = new
+      {
+        name,
+        value
+      };
+    }
+
+    private static bool ShouldAddAntiForgeryToken(ExtensibleRenderedJsonRendering extensibleRendering)
+    {
+      var rendering = extensibleRendering?.Rendering?.RenderingItem?.InnerItem;
+      if (rendering == null) return false;
+
+      var addAntiForgeryTokenField = (CheckboxField)rendering.Fields[Templates.JsonRendering.Fields.AddAntiForgeryToken];
+      return addAntiForgeryTokenField?.Checked ?? false;
+    }
+
+    private static (string Name, string Value) ParseAntiForgeryToken(HtmlString antiForgeryTokenHtml)
+    {
+      var doc = new HtmlDocument();
+      doc.LoadHtml(antiForgeryTokenHtml.ToString());
+      var input = doc.DocumentNode.SelectSingleNode("//input");
+      return (input.Attributes["name"].Value, input.Attributes["value"].Value);
+    }
+  }
+}
